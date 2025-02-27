864f54b07ed7ec7f361793c8dafbd45df8a8ff13
CodewithMK180105 <manishkumar180105@gmail.com>
2025-02-27 20:53:50 +0530

    Added Frontend Skills.

---
+++ components/Skills.tsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import { InfiniteMovingCards } from './ui/infinite-moving-cards';
+
+interface skillType{
+  path: string,
+  skill: string,
+}
+
+const skillTypeArray: skillType[] = [
+    {
+      path: '/next.svg',
+      skill: 'Next JS',
+    },
+    {
+      path: '/React.svg',
+      skill: 'React JS',
+    },
+    {
+      path: '/ts.svg',
+      skill: 'TypeScript',
+    },
+    {
+      path: '/Js.svg',
+      skill: 'JavaScript',
+    },
+    {
+      path: '/tail.svg',
+      skill: 'Tailwind',
+    },
+    {
+      path: '/bootstrap.svg',
+      skill: 'Bootstrap',
+    },
+    {
+      path: '/css.svg',
+      skill: 'CSS',
+    },
+    {
+      path: '/html.svg',
+      skill: 'HTML',
+    },
+  ];
+
+const Skills = () => {
+  return (
+    <div className='h-[10rem] w-full bg-grid-black/[0.15] dark:bg-black dark:bg-grid-white/[0.15] relative flex flex-col items-center justify-center overflow-hidden'>
+      <h1 className="heading mb-4">
+        My
+        <span className="text-purple"> Tech Skills</span>
+      </h1>
+      <div className='flex justify-center w-full overflow-hidden px-4 sm:px-6 lg:px-8'>
+        <div className='w-full'>
+        <InfiniteMovingCards
+            items={skillTypeArray}
+            direction="right"
+            speed="fast"
+        />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Skills
